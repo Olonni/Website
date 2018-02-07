23a939ead82116648d0744ed777d9a40d13292c6
Olonni <drm36@live.com>
2018-02-06 22:00:10 -0500

    Initial Setup

---
+++ contact.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+
+        <title>David Maisch</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Iceland" />      
+        <link rel="shortcut icon" href="/lgoo.png">
+</head>
+<body>
+        <div id="logoImage">
+                <img src="lgoo.png" alt="logo"style="position:relative;  height:160px; width:160px;">
+        </div>
+        <div class="navbar">
+                <a href="http://www.olonni.com/">Home</a>
+                <a href="http://www.olonni.com/about.php">About</a>
+                <a href="http://www.olonni.com/contact.php">Contact</a>
+        </div>
+        <h1>DAVID MAISCH</h1>  <br>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,55 @@
+<html>
+
+<head>
+
+        <title>David Maisch</title>
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Iceland" />      
+        <link rel="shortcut icon" href="/lgoo.png">
+</head>
+<body>
+        <div id="logoImage">
+                <img src="lgoo.png" alt="logo"style="position:relative;  height:160px; width:160px;">
+        </div>
+        <div class="navbar">
+                <a href="http://www.olonni.com/">Home</a>
+                <a href="http://www.olonni.com/about.php">About</a>
+                <a href="http://www.olonni.com/contact.php">Contact</a>
+        </div>
+        <h1>DAVID MAISCH</h1>  <br>
+        <h2>WHO AM I.</h2>
+        <p>
+                I'm a software engineer. I help people fix their <b>software</b> issues; so they don't have to.
+        </p>
+
+        <br>
+        <h2>SKILLS.</h2>
+           <div class="skills">
+                <p>LANGUAGES; </p>
+                <ul>
+                <p>COMPETENT: Java, SQL, HTMl, CSS, Lua, Perl</p>
+                <p>SOME KNOWLEDGE: C, C++, Python, JavaScript, COBOL</p>
+                </ul>
+
+
+                <p>SOFTWARE; </p>
+                <ul>
+                <p>COMPETENT: Notepad++, Eclipse, MySQL, Vim</p>
+                </ul>
+
+
+                <p>OPERATING SYSTEMS;</p>
+                <ul>
+                <p>COMPETENT: Linux, Windows, MacOS</p>
+                </ul>
+           </div>
+ <br>
+        <h2>LINKS.</h2>
+        <ul>
+                <li><a href="https://www.linkedin.com/in/DavidMaisch">LinkedIn</a></li>
+                <li><a href="https://www.twitter.com/Olonni">Twitter</a></li>
+                <li><a href="https://www.github.com/Olonni">Github</a></li>
+                <li><a href="/DavidMaischResume.docx" download>Resume</a></li>
+        </ul>
+</body>
+</html>
